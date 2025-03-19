b3bd3f35ee0d85f556cb9e5518b55be3ead7623f
AndrewTaker <n0thingg@yandex.ru>
2025-03-19 22:50:58 +0300

    add routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -4,7 +4,8 @@ from pydantic import ValidationError
 from app.services.product_service import ProductService
 from app.models.schemas import ProductCreate, ProductResponse
 from app.routes.errors import validation_error, generic_error
-from app.utilities.cache import Cache
+from app.utilities import cache
+from app.utilities.cache import Entry
 
 api_v1_blueprint = Blueprint('api/v1', __name__)
 
@@ -20,9 +21,16 @@ def create_product():
         return generic_error()
 
 @api_v1_blueprint.route('/products/<int:product_id>', methods=['GET'])
-def get_product(product_id):
+def get_product(product_id: int):
+    cache_key = f"products:{product_id}"
     try:
-        product = ProductService.get_product(product_id)
+        cached = cache.get(cache_key)
+        if cached:
+            product = cached 
+        else:
+            product = ProductService.get_product(product_id)
+            cache.set(cache_key, Entry(product))
+
         return jsonify(ProductResponse(**product).model_dump()), HTTPStatus.OK
     except ValidationError as e:
         return validation_error(e)
@@ -31,13 +39,36 @@ def get_product(product_id):
 
 @api_v1_blueprint.route('/products', methods=['GET'])
 def get_all_products():
+    cache_key = "products"
     try:
-        products = ProductService.get_all_products()
+        cached = cache.get(cache_key)
+        if cached:
+            products = cached
+        else:
+            products = ProductService.get_all_products()
+            cache.set(cache_key, Entry(products))
         return (
-                jsonify([ProductResponse(**product).model_dump() for product in products]),
-                HTTPStatus.OK
+            jsonify({
+                "limit": 0,
+                "offset": 0,
+                "data": [ProductResponse(**product).model_dump() for product in products]
+            }),
+            HTTPStatus.OK
         )
     except ValidationError as e:
         return validation_error(e)
     except Exception as e:
+        return generic_error()
+
+@api_v1_blueprint.route('/products/<int:product_id>', methods=['PUT'])
+def full_update_product(product_id: int):
+    try:
+        product_update = ProductCreate(**request.get_json())
+        product = ProductService.full_update_product(product_id, product_update)
+        return jsonify(ProductResponse(**product).model_dump()), HTTPStatus.OK
+    except ValidationError as e:
+        print(e)
+        return validation_error(e)
+    except Exception as e:
+        print(e)
         return generic_error()
