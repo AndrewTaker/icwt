83caabc35b90eb2b1843dcf7ce9dcc6127348e53
AndrewTaker <n0thingg@yandex.ru>
2025-03-20 02:25:10 +0300

    add versioning to api prefix

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.routes.api import api_v1_blueprint
 
 app = Flask(__name__)
 app.config.from_object('app.config.Config')
-app.register_blueprint(api_v1_blueprint, url_prefix="/api")
+app.register_blueprint(api_v1_blueprint, url_prefix="/api/v1")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
