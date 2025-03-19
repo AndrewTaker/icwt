7e7051abe4dde94e820d2b7fdf891a541533e2bb
AndrewTaker <n0thingg@yandex.ru>
2025-03-19 18:45:41 +0300

    create and update api handlers

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,30 +1,43 @@
+from http import HTTPStatus
 from flask import Blueprint, request, jsonify
+from pydantic import ValidationError
 from app.services.product_service import ProductService
 from app.models.schemas import ProductCreate, ProductResponse
-from app.routes.errors import handle_validation_error
+from app.routes.errors import validation_error, generic_error
+from app.utilities.cache import Cache
 
-api_blueprint = Blueprint('api', __name__)
+api_v1_blueprint = Blueprint('api/v1', __name__)
 
-@api_blueprint.route("/kek", methods=['GET'])
-def return_kek():
-    return jsonify({"hi": "there"}), 200
-
-@api_blueprint.route('/products', methods=['POST'])
+@api_v1_blueprint.route('/products', methods=['POST'])
 def create_product():
-    data = request.json
-    product_data = ProductCreate(**data)
-    new_product = ProductService.create_product(product_data)
-    return jsonify(ProductResponse(**new_product).model_dump()), 201
+    try:
+        product_data = ProductCreate(**request.get_json())
+        new_product = ProductService.create_product(product_data)
+        return jsonify(ProductResponse(**new_product).model_dump()), HTTPStatus.CREATED
+    except ValidationError as e:
+        return validation_error(e)
+    except Exception as e:
+        return generic_error()
 
-@api_blueprint.route('/products/<int:product_id>', methods=['GET'])
+@api_v1_blueprint.route('/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
     try:
         product = ProductService.get_product(product_id)
-        return jsonify(ProductResponse(**product).model_dump()), 200
+        return jsonify(ProductResponse(**product).model_dump()), HTTPStatus.OK
+    except ValidationError as e:
+        return validation_error(e)
     except Exception as e:
-        return handle_validation_error(e)
+        return generic_error()
 
-@api_blueprint.route('/products', methods=['GET'])
+@api_v1_blueprint.route('/products', methods=['GET'])
 def get_all_products():
-    products = ProductService.get_all_products()
-    return jsonify([ProductResponse(**product).model_dump() for product in products]), 200
+    try:
+        products = ProductService.get_all_products()
+        return (
+                jsonify([ProductResponse(**product).model_dump() for product in products]),
+                HTTPStatus.OK
+        )
+    except ValidationError as e:
+        return validation_error(e)
+    except Exception as e:
+        return generic_error()
