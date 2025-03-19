0a776f2093d7d43163d7014ae2619dad956a4c57
AndrewTaker <n0thingg@yandex.ru>
2025-03-19 16:50:41 +0300

    add config for app

---
+++ app/config.py
@@ -0,0 +1,14 @@
+import os
+
+PSQL_CONFIG = {
+    'dbname': os.getenv("PSQL_DB_NAME", "dev"),
+    'user': os.getenv("PSQL_USER", "dev"),
+    'password': os.getenv("PSQL_PASSWORD", "dev"),
+    'host': os.getenv("PSQL_HOST", "localhost"),
+    'port': os.getenv("PSQL_PORT", 5432),
+}
+
+class Config:
+    DEBUG = True
+    SECRET_KEY = 'test'
+    DATABASE_URL = "dbname={dbname} user={user} password={password} host={host} port={port}".format(**PSQL_CONFIG)
