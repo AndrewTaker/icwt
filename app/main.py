e4d9b087a217842dff25d3fcee4e69eea7eb019e
AndrewTaker <n0thingg@yandex.ru>
2025-03-19 22:52:24 +0300

    refactor main file, get rid of init `create_app` function

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from app.routes.api import api_v1_blueprint
+
+app = Flask(__name__)
+app.config.from_object('app.config.Config')
+app.register_blueprint(api_v1_blueprint, url_prefix="/api")
+
+if __name__ == "__main__":
+    app.run(debug=True)
