dec40bb9f91d1119bdd384ef916c86557950288b
AndrewTaker <n0thingg@yandex.ru>
2025-03-20 15:32:32 +0300

    add error handler for dates errors, add debug to generic error handler

--- app/routes/errors.py
+++ app/routes/errors.py
@@ -3,10 +3,11 @@ from http import HTTPStatus as status_code
 from flask import Response, jsonify
 
 
-def generic_error() -> tuple[Response, int]:
+def generic_error(e: Exception = ValueError("qq")) -> tuple[Response, int]:
     return jsonify({
         "error": "Invalid data",
-        "details": "Something went wrong"
+        "details": "Something went wrong",
+        "debug_err": str(e),
     }), status_code.BAD_REQUEST.value
 
 
@@ -14,4 +15,11 @@ def validation_error(error: Exception) -> tuple[Response, int]:
     return jsonify({
         "error": "Invalid data",
         "details": str(error)
+    }), status_code.BAD_REQUEST.value
+
+
+def date_range_error(start_date: str, end_date: str) -> tuple[Response, int]:
+    return jsonify({
+        "error": "Invalid date range",
+        "details": f"Start_date must be earlier than end_date {start_date=} {end_date=}"
     }), status_code.BAD_REQUEST.value
