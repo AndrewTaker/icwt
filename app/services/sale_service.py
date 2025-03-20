3e6b508601732f8128e2dab81dbe52717cf05eeb
AndrewTaker <n0thingg@yandex.ru>
2025-03-20 15:45:40 +0300

    add service to query top n products

--- app/services/sale_service.py
+++ app/services/sale_service.py
@@ -23,3 +23,27 @@ class SaleService:
                     raise(ValueError("get_total_for_period err: ValueError"))
 
                 return total
+
+    @staticmethod
+    def get_n_top_products(start_date: str, end_date: str, limit: int):
+        """
+        Retrieve all products using raw SQL.
+        """
+# select product.name, sale.quantity from product join sale on sale.product_id = product.id where sale.sale_date between '2023-10-01' and '2023-10-30' order by sale.quantity DESC limit 5;
+        query = """
+        SELECT product.name, sale.quantity
+        FROM product
+        JOIN sale ON sale.product_id = product.id
+        WHERE sale_date BETWEEN %s AND %s
+        ORDER BY sale.quantity DESC
+        LIMIT %s
+        """
+        with get_db_connection() as conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                cursor.execute(query, (start_date, end_date, limit))
+                total = cursor.fetchone()
+
+                if not total:
+                    raise(ValueError("get_total_for_period err: ValueError"))
+
+                return total
