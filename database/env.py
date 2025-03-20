0afd516536de95cc0f7e671b5f98d5f416ecf4b0
AndrewTaker <n0thingg@yandex.ru>
2025-03-20 03:02:31 +0300

    fix format in alembic env file

--- database/env.py
+++ database/env.py
@@ -18,30 +18,30 @@ if config.config_file_name is not None:
 
 target_metadata = None
 
+
 def run_migrations_online() -> None:
     connectable = create_engine(DSN)
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         migrations_directory = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), 'migrations')
+            os.path.join(os.path.dirname(__file__), "migrations")
         )
         done_dicrectory = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), 'migrations', 'done')
+            os.path.join(os.path.dirname(__file__), "migrations", "done")
         )
 
         with connection.begin():
             for file in sorted(os.listdir(migrations_directory)):
-                if file.endswith('.sql') and file not in os.listdir(done_dicrectory):
+                if file.endswith(".sql") and file not in os.listdir(done_dicrectory):
                     sql_file_path = os.path.join(migrations_directory, file)
-                    with open(sql_file_path, 'r') as f:
+                    with open(sql_file_path, "r") as f:
                         sql_script = f.read()
                         connection.exec_driver_sql(sql_script)
                         print(f"migrate -> {sql_file_path}")
 
             context.get_bind().commit()
+
 
 run_migrations_online()
