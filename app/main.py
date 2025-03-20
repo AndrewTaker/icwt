ba6c75920b33d5eef45036fb6e898aa0a0c7d681
AndrewTaker <n0thingg@yandex.ru>
2025-03-20 04:07:14 +0300

    wrap app creatio into a function, separate routes by groups

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
-from app.routes.api import api_v1_blueprint
+from app.routes.api import products_blueprint, sales_blueprint
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object("app.config.Config")
+    api_v1_blueprint = Blueprint("api_v1", __name__, url_prefix="/api/v1")
+    api_v1_blueprint.register_blueprint(products_blueprint, url_prefix="/products")
+    api_v1_blueprint.register_blueprint(sales_blueprint, url_prefix="/sales")
+    app.register_blueprint(api_v1_blueprint)
+    return app
+
+
+def print_routes(app: Flask) -> None:
+    for rule in app.url_map.iter_rules():
+        print(rule, rule.methods)
+
 
 app = Flask(__name__)
-app.config.from_object('app.config.Config')
-app.register_blueprint(api_v1_blueprint, url_prefix="/api/v1")
 
 if __name__ == "__main__":
-    app.run()
+    app = create_app()
+    print_routes(app)
+    app.run(debug=True)
